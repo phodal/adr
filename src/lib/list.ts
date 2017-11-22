a10def33e00fb55ec8c5219807b46577c12d383c
Phodal Huang <h@phodal.com>
2017-11-22 17:40:59 +0800

    [LIST] use table to befautiful

--- src/lib/list.ts
+++ src/lib/list.ts
@@ -1,27 +1,34 @@
 let walk = require('walk')
 let fs = require('fs')
 let moment = require('moment')
+let {table} = require('table')
 let {getSavePath} = require('./utils')
 
 export function list () {
   let path = getSavePath()
   let walker = walk.walk(path, {})
+  let output
+  let tableData = [
+  ['决策', '上次修改时间']
+];
 
-  console.log('|                  决策         |  上次修改时间|')
-  console.log('|------------------------------| -----------|')
   walker.on('file', function (root, fileStats, next) {
-    if(!fileStats.name) {
-      next();
+    if (!fileStats.name) {
+      next()
     }
     let fileName = fileStats.name
-    let fileNameLength = fileStats.name.length
-    let numberLength = 4;
-    let markdownWifhPrefixLength = 3;
+    let fileNameLength = fileName.length
+    let numberLength = 4
+    let markdownWifhPrefixLength = 3
     let decision = fileStats.name.substring(numberLength, fileNameLength - markdownWifhPrefixLength)
-    console.log('| ' + decision + ' | ' +  moment(fileStats.mtime).format('YYYY-MM-DD')  + '|')
+    tableData.push(
+      [decision, moment(fileStats.mtime).format('YYYY-MM-DD')]
+    )
+    output = table(tableData)
+    console.log(output)
   })
 
-  walker.on("end", function () {
-    console.log("all done")
+  walker.on('end', function () {
+    console.log(output)
   })
 }
