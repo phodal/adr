67057ba8d51cf71229f194e8c88881f8409c8d1a
Phodal Huang <h@phodal.com>
2017-11-22 17:56:58 +0800

    [T] list done

--- src/lib/list.ts
+++ src/lib/list.ts
@@ -1,4 +1,4 @@
-let walk = require('walk')
+let walkSync = require('walk-sync');
 let fs = require('fs')
 let moment = require('moment')
 let {table} = require('table')
@@ -6,29 +6,24 @@ let {getSavePath} = require('./utils')
 
 export function list () {
   let path = getSavePath()
-  let walker = walk.walk(path, {})
   let output
   let tableData = [
-  ['决策', '上次修改时间']
-];
+    ['决策', '上次修改时间']
+  ];
 
-  walker.on('file', function (root, fileStats, next) {
-    if (!fileStats.name) {
-      next()
-    }
-    let fileName = fileStats.name
+  let files = walkSync.entries(path)
+  for(let i=0;i < files.length; i++) {
+    let file = files[i];
+    let fileName = file.relativePath
     let fileNameLength = fileName.length
     let numberLength = 4
     let markdownWifhPrefixLength = 3
-    let decision = fileStats.name.substring(numberLength, fileNameLength - markdownWifhPrefixLength)
+
+    let decision = fileName.substring(numberLength, fileNameLength - markdownWifhPrefixLength)
     tableData.push(
-      [decision, moment(fileStats.mtime).format('YYYY-MM-DD')]
+      [decision, moment(file.mtime).format('YYYY-MM-DD')]
     )
     output = table(tableData)
-    console.log(output)
-  })
-
-  walker.on('end', function () {
-    console.log(output)
-  })
+  }
+  console.log(output)
 }
