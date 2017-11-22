03ed2edc1a201b28464f48bcb088b3fb59f20657
Phodal Huang <h@phodal.com>
2017-11-22 16:49:48 +0800

    [T] it works

--- src/lib/new.ts
+++ src/lib/new.ts
@@ -1,7 +1,9 @@
 let fs = require('fs')
 let path = require('path')
+let workDir = process.cwd()
+let mkdirp = require('mkdirp')
 
-export function create (name: string) {
+let createDecisions = function (name: string, savePath: string | any | void) {
   let raw = fs.readFileSync(__dirname + path.normalize('/template.md'), 'utf8')
   let dateObj = new Date()
   let month = dateObj.getUTCMonth() + 1
@@ -14,5 +16,28 @@ export function create (name: string) {
     .replace(/{DATE}/g, newDate)
     .replace(/{STATUS}/g, '状态')
 
-  fs.writeFileSync('001-hello' + '.md', result)
+  fs.writeFileSync(savePath + '001-hello' + '.md', result)
+};
+
+export function create (name: string) {
+  let savePath = getSavePath()
+  console.log('保存的路径是：' + savePath)
+  mkdirp(savePath, function (err) {
+    if (err) console.error(err)
+    else createDecisions(name, savePath)
+  })
+}
+
+function getSavePath () {
+  if (!fs.existsSync(workDir + '/.adr.json')) {
+    return workDir + '/doc/ard/'
+  }
+
+  let config = fs.readFileSync(workDir + '/.adr.json', 'utf8')
+
+  try {
+    return JSON.parse(config).path
+  } catch (e) {
+    return console.error(e)
+  }
 }
