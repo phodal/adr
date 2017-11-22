6bb91795cdce996dbf534e58fe987de081566bb1
Phodal Huang <h@phodal.com>
2017-11-22 11:15:21 +0800

    [T] simplify code

--- src/lib/number.spec.ts
+++ src/lib/number.spec.ts
@@ -1,10 +1,6 @@
 import { test } from 'ava'
-import { double, power } from 'typescript-starter'
+import { double } from 'adr'
 
 test('double', t => {
   t.deepEqual(double(2), 4)
-})
-
-test('power', t => {
-  t.deepEqual(power(2,4), 16)
 })
