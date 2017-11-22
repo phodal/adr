9cb11887cf8016758d0794bf83b90f982cde556b
Fengda HUANG <h@phodal.com>
2017-11-22 22:49:41 +0800

    [GENERATE] add graph

--- src/lib/generate.ts
+++ src/lib/generate.ts
@@ -25,12 +25,38 @@ function generateToc () {
   return output
 }
 
+function generateGraph () {
+  let path = Utils.getSavePath()
+  let output = 'digraph {\n  node [shape=plaintext];'
+  let files = walkSync.entries(path)
+  for (let i = 0;i < files.length; i++) {
+    let file = files[i]
+    let fileName = file.relativePath
+    let fileNameLength = fileName.length
+    let numberLength = 4
+    let markdownWithPrefixLength = 3
+
+    let index = parseInt(fileName.substring(0, 3), 10)
+    if (index) {
+      let decision = fileName.substring(numberLength, fileNameLength - markdownWithPrefixLength)
+      output = output + '\n  _' + index + ' [label="' + index + '.' + decision + '"; URL="' + file.relativePath + '"]'
+
+      if (index !== 1) {
+        output = output + '\n  _' + (index - 1) + ' -> _' + index + ' [style="dotted"];'
+      }
+    }
+  }
+  output = output + '\n}\n'
+  console.log(output)
+  return output
+}
+
 export function generate (type) {
   if (type === 'toc') {
     return generateToc()
   }
   if (type === 'graph') {
-    return generateToc()
+    return generateGraph()
   }
 
   console.log('\n error: type ' + type + ' current not supported')
