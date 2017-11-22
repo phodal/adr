3b5f2e585ce94820891625d72a14e2f629598d6d
Phodal Huang <h@phodal.com>
2017-11-22 17:02:01 +0800

    [CREATE] generate file name

--- src/lib/new.ts
+++ src/lib/new.ts
@@ -3,21 +3,27 @@ let path = require('path')
 let workDir = process.cwd()
 let mkdirp = require('mkdirp')
 
-let createDecisions = function (name: string, savePath: string | any | void) {
+function generateFileName (originFileName) {
+  return originFileName.replace(/，/g, '')
+    .replace(/。/g, '')
+}
+
+function createDecisions (name: string, savePath: string | any | void) {
   let raw = fs.readFileSync(__dirname + path.normalize('/template.md'), 'utf8')
   let dateObj = new Date()
   let month = dateObj.getUTCMonth() + 1
   let day = dateObj.getUTCDate()
   let year = dateObj.getUTCFullYear()
   let newDate = year + '/' + month + '/' + day
+  let fileName = generateFileName(name)
 
   let result = raw.replace(/{NUMBER}/g, '1')
     .replace(/{TITLE}/g, name)
     .replace(/{DATE}/g, newDate)
     .replace(/{STATUS}/g, '状态')
 
-  fs.writeFileSync(savePath + '001-hello' + '.md', result)
-};
+  fs.writeFileSync(savePath + '001-' + fileName + '.md', result)
+}
 
 export function create (name: string) {
   let savePath = getSavePath()
