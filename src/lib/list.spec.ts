401bec5f0a2200f4902798caa03834c83e9e8ca1
Fengda HUANG <h@phodal.com>
2017-11-23 21:48:09 +0800

    [TEST] list: test for include README.md

--- src/lib/list.spec.ts
+++ src/lib/list.spec.ts
@@ -28,3 +28,33 @@ test('ADR: list', t => {
   entriesSpy.restore()
   consoleSpy.restore()
 })
+
+test('ADR: list with README.md', t => {
+  let ADRGetSavePathSpy = sinon.stub(Utils, 'getSavePath').returns('./')
+  let consoleSpy = sinon.stub(console, 'log')
+  let entriesSpy = sinon.stub(walkSync, 'entries').returns([{
+    relativePath: '001-编写完整的单元测试.md',
+    basePath: '/Users/fdhuang/learing/adr/doc/ard/',
+    mode: 33188,
+    size: 246,
+    mtime: 1511435254653 },
+    {
+      relativePath: 'README.md',
+      basePath: '/Users/fdhuang/learing/adr/doc/ard/',
+      mode: 33188,
+      size: 246,
+      mtime: 1511435254653 }
+  ])
+
+  let results = ADR.list()
+  t.deepEqual(results,
+`╔══════════════════════╤══════════════╗
+║ 决策                 │ 上次修改时间 ║
+╟──────────────────────┼──────────────╢
+║ 1.编写完整的单元测试 │ 2017-11-23   ║
+╚══════════════════════╧══════════════╝
+`)
+  ADRGetSavePathSpy.restore()
+  entriesSpy.restore()
+  consoleSpy.restore()
+})
