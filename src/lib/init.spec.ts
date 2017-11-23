f0fbf18d39cee6b9316a76a4d71832b87d4f5e9a
Fengda HUANG <h@phodal.com>
2017-11-23 22:07:23 +0800

    [INIT] add test for multi language

--- src/lib/init.spec.ts
+++ src/lib/init.spec.ts
@@ -3,7 +3,18 @@ let fs = require('fs')
 import { test } from 'ava'
 import ADR from 'adr'
 
-test('ADR: init', t => {
+test('ADR: init in chinese', t => {
+  let cwdSpy = sinon.stub(process, 'cwd').returns('/test')
+  let fsWriteSpy = sinon.stub(fs, 'writeFileSync')
+
+  ADR.init('chinese')
+  t.deepEqual(fsWriteSpy.calledOnce, true)
+  t.deepEqual(fsWriteSpy.calledWith('/test/.adr.json', JSON.stringify({ path: 'doc/ard/', language: 'zh-cn' })), true)
+  cwdSpy.restore()
+  fsWriteSpy.restore()
+})
+
+test('ADR: init en', t => {
   let cwdSpy = sinon.stub(process, 'cwd').returns('/test')
   let fsWriteSpy = sinon.stub(fs, 'writeFileSync')
 
