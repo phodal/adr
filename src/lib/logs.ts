89deda61d83e52e82346a0a01cbf946cefe9bc20
Phodal Huang <h@phodal.com>
2017-11-24 15:37:31 +0800

    [LOGS] add show logs

--- src/lib/logs.ts
+++ src/lib/logs.ts
@@ -1,3 +1,34 @@
+let walkSync = require('walk-sync')
+
+import Utils from './utils'
+import Status from './status'
+
+let path = Utils.getSavePath()
+
+let getAllFilesName = function () {
+  let outputArray = ['']
+  let files = walkSync.entries(path)
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i]
+    let fileName = file.relativePath
+
+    let index = parseInt(fileName.substring(0, Utils.DEFAULT_DIGITS), 10)
+    if (index) {
+      outputArray[index] = fileName
+    }
+  }
+
+  return outputArray
+}
+
 export function logs (index) {
-  return index
+  let outputArray = getAllFilesName()
+  let currentFileName = outputArray[index]
+  let filePath = path + currentFileName
+  let allStatus = Status.getAllStatus(filePath)
+  for (let i = 0; i < allStatus.length; i++) {
+    let currentStatus = allStatus[i]
+    console.log(currentStatus)
+  }
+  return allStatus
 }
