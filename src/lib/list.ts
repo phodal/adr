f4b5bcde1e4b0c3b406680b6b2a4fe287b8c78ba
Fengda HUANG <h@phodal.com>
2017-11-25 21:05:12 +0800

    [LIST] simpleify code

--- src/lib/list.ts
+++ src/lib/list.ts
@@ -6,12 +6,10 @@ import Utils from './utils'
 import Status from './status'
 
 export function list (): string {
-  let path = Utils.getSavePath()
   let output
+  let path = Utils.getSavePath()
   let i18n = Utils.getI18n()
-  let tableData = [
-    [i18n.decision, i18n.modifiedDate, i18n.lastStatus]
-  ]
+  let tableData = [[i18n.decision, i18n.modifiedDate, i18n.lastStatus]]
 
   let files = walkSync.entries(path)
   for (let i = 0;i < files.length; i++) {
@@ -20,14 +18,11 @@ export function list (): string {
     if (fileName === 'README.md') {
       break
     }
-    let fileNameLength = fileName.length
     let numberLength = Utils.getNumberLength(fileName) + '-'.length
-    let markdownWithPrefixLength = '.md'.length
-
     let index = Utils.getIndexByString(fileName)
-    let lastStatus = Status.getLatestStatus(path + fileName)
     if (index) {
-      let decision = fileName.substring(numberLength, fileNameLength - markdownWithPrefixLength)
+      let lastStatus = Status.getLatestStatus(path + fileName)
+      let decision = fileName.substring(numberLength, fileName.length - '.md'.length)
       let body = [index + '.' + decision, moment(file.mtime).format('YYYY-MM-DD'), lastStatus]
       tableData.push(body)
     }
