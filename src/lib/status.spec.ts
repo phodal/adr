46e7114d221d09b0ccd8c6b78eba06d0ba0022a7
Phodal HUANG <h@phodal.com>
2017-11-25 17:32:11 +0800

    [TEST] try to write status teste

---
+++ src/lib/status.spec.ts
@@ -0,0 +1,30 @@
+// let sinon = require('sinon')
+// let fs = require('fs')
+import { test } from 'ava'
+
+import ADR from 'adr'
+// let Status = ADR.Status
+
+// test('ADR: init in chinese', t => {
+//   let fsReadSpy = sinon.stub(fs, 'readFileSync').returns(
+// `
+// # 10. 更友好的 CLI
+//
+// 日期: 2017-11-23
+//
+// ## 状态
+//
+// 列表：提议/通过/完成/已弃用/已取代
+//
+// 2017-11-23 提议
+// `
+//   )
+//
+//   let status = Status.getAllStatus('./001-README.md')
+//   t.deepEqual(status, [''])
+//   fsReadSpy.restore()
+// })
+
+test('placeholder', t => {
+  t.deepEqual(true, true)
+})
