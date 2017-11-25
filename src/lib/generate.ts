14aef80717b5bdb4dc2e8ea77590138d1b9ca8ac
Phodal HUANG <h@phodal.com>
2017-11-25 16:46:13 +0800

    [Generate] extract build func

--- src/lib/generate.ts
+++ src/lib/generate.ts
@@ -1,18 +1,27 @@
 import {GenerateBuilder} from './GenerateBuilder'
 import Utils from './utils'
 
+function buildGraphBuildFun (index, decision, filePath, bodyString, filesLength) {
+  bodyString[index] = '\n  _' + index + ' [label="' + index + '.' + decision + '"; URL="' + filePath + '"]'
+  if (index !== 1) {
+    bodyString[filesLength + index] = '\n  _' + (index - 1) + ' -> _' + index + ' [style="dotted"];'
+  }
+  return bodyString
+}
+
+function buildTocBodyFun (index, decision, filePath, bodyString) {
+  bodyString[index] = '\n* [' + index + '. ' + decision + '](' + filePath + ')'
+  return bodyString
+}
+
 function generateToc (options?: object) {
   let path = Utils.getSavePath()
   let graphGenerate = new GenerateBuilder(path)
   let header = '# Architecture Decision Records\n'
-  let buildBodyFunc = function (index, decision, filePath, bodyString) {
-    bodyString[index] = '\n* [' + index + '. ' + decision + '](' + filePath + ')'
-    return bodyString
-  }
   let results = graphGenerate
     .setStartString(header)
     .setEndString('')
-    .buildBody(buildBodyFunc)
+    .buildBody(buildTocBodyFun)
     .build()
 
   if (!(!!options && options['output'])) {
@@ -25,17 +34,10 @@ function generateGraph () {
   let path = Utils.getSavePath()
   let graphGenerate = new GenerateBuilder(path)
   let header = 'digraph {\n  node [shape=plaintext];'
-  let buildBodyFunc = function (index, decision, filePath, bodyString, filesLength) {
-    bodyString[index] = '\n  _' + index + ' [label="' + index + '.' + decision + '"; URL="' + filePath + '"]'
-    if (index !== 1) {
-      bodyString[filesLength + index] = '\n  _' + (index - 1) + ' -> _' + index + ' [style="dotted"];'
-    }
-    return bodyString
-  }
   let results = graphGenerate
     .setStartString(header)
     .setEndString('\n}\n')
-    .buildBody(buildBodyFunc)
+    .buildBody(buildGraphBuildFun)
     .build()
 
   console.log(results)
