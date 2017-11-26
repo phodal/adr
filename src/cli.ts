fb5a201fa2bc43fd1ce5cc455ae1825639016a55
Fengda HUANG <h@phodal.com>
2017-11-26 15:18:43 +0800

    [export] create cli portal

--- src/cli.ts
+++ src/cli.ts
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 
 let program = require('commander')
+let colors = require('colors')
 let version = require('../../package.json').version
+
 let {create} = require('./lib/create')
 let {list} = require('./lib/list')
 let {generate} = require('./lib/generate')
 let {update} = require('./lib/update')
 let {init} = require('./lib/init')
 let {logs} = require('./lib/logs')
-let colors = require('colors')
+let {output} = require('./lib/output')
 
 program
   .version(version)
@@ -19,6 +21,7 @@ program
   .option('-g, generate <type>', 'generate toc or graph, default toc', generate)
   .option('-init, init <language>', 'init ADR with language, e.g. ``adr init en``', init)
   .option('-logs, logs <index>', 'list one ADR status logs', logs)
+  .option('-o, export <format>', 'export ADR in CSV, JSON', output)
 
 program.parse(process.argv)
 
