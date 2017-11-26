fa3e26267d7fb9b5e64a17d2d17124e92249c9f7
Fengda HUANG <h@phodal.com>
2017-11-26 22:48:41 +0800

    [Output] use generate

--- src/lib/output.ts
+++ src/lib/output.ts
@@ -1,38 +1,36 @@
-let walkSync = require('walk-sync')
 let moment = require('moment')
 let fs = require('fs')
 
 import Utils from './utils'
 import Status from './status'
+import {GenerateBuilder} from './base/GenerateBuilder'
 
-function generateCsv () {
+let path = Utils.getSavePath()
+function buildTocBodyFun (index, decision, file, bodyString): string[] {
+  let lastStatus = Status.getLatestStatus(path + file.relativePath)
+  let body = `${index}, ${decision}, ${moment(file.mtime).format('YYYY-MM-DD')}, ${lastStatus}\n`
+  return bodyString.push(body)
+}
+
+function listAdr () {
   let path = Utils.getSavePath()
   let i18n = Utils.getI18n()
-  let csvString = `Index, ${i18n.decision}, ${i18n.modifiedDate}, ${i18n.lastStatus}\n`
-  let files = walkSync.entries(path)
-  for (let i = 0; i < files.length; i++) {
-    let file = files[i]
-    let fileName = file.relativePath
-    if (fileName === 'README.md') {
-      break
-    }
-    let numberLength = Utils.getNumberLength(fileName) + '-'.length
-    let index = Utils.getIndexByString(fileName)
-    if (index) {
-      let decision = fileName.substring(numberLength, fileName.length - '.md'.length)
-      let lastStatus = Status.getLatestStatus(path + fileName)
-      let body = `${index}, ${decision}, ${moment(file.mtime).format('YYYY-MM-DD')}, ${lastStatus}\n`
-      csvString = csvString + body
-    }
-  }
+  let graphGenerate = new GenerateBuilder(path)
+  let startString = `Index, ${i18n.decision}, ${i18n.modifiedDate}, ${i18n.lastStatus}\n`
+  let results = graphGenerate
+    .setStartString(startString)
+    .setEndString('')
+    .buildBody(buildTocBodyFun)
+    .build()
 
-  return csvString
+  return results
 }
 
 export function output (type: string): string {
   let output
   if (type === 'csv') {
-    output = generateCsv()
+    output = listAdr()
+    console.log(output)
     let workDir = Utils.getWorkDir()
     fs.writeFileSync(workDir + '/export.csv', output, 'utf-8')
   }
