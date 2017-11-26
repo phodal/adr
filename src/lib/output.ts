0280fb69b590f59006f0b8a7da31357d9eb76122
Fengda HUANG <h@phodal.com>
2017-11-26 15:49:43 +0800

    [export] add simple handle

--- src/lib/output.ts
+++ src/lib/output.ts
@@ -1,3 +1,30 @@
-export function output (): string[] {
-  return ['']
+let walkSync = require('walk-sync')
+let moment = require('moment')
+
+import Utils from './utils'
+import Status from './status'
+
+export function output (): string {
+  let path = Utils.getSavePath()
+  let i18n = Utils.getI18n()
+  let csvString = `Index, ${i18n.decision}, ${i18n.modifiedDate}, ${i18n.lastStatus}\n`
+  let files = walkSync.entries(path)
+  for (let i = 0;i < files.length; i++) {
+    let file = files[i]
+    let fileName = file.relativePath
+    if (fileName === 'README.md') {
+      break
+    }
+    let numberLength = Utils.getNumberLength(fileName) + '-'.length
+    let index = Utils.getIndexByString(fileName)
+    if (index) {
+      let lastStatus = Status.getLatestStatus(path + fileName)
+      let decision = fileName.substring(numberLength, fileName.length - '.md'.length)
+      let body = `${index}, ${decision}, ${moment(file.mtime).format('YYYY-MM-DD')}, ${lastStatus}\n`
+      csvString = csvString + body
+    }
+  }
+  console.log(csvString)
+
+  return csvString
 }
