25af69e0ec68d074f5b6bdee04aa700861aaea25
Phodal HUANG <h@phodal.com>
2017-11-29 17:43:47 +0800

    [T] remove unneed code

--- src/lib/output.ts
+++ src/lib/output.ts
@@ -4,31 +4,11 @@ let walkSync = require('walk-sync')
 
 import Utils from './utils'
 import Status from './status'
-import {GenerateBuilder} from './base/GenerateBuilder'
 import {JsonGenerateBuilder} from './base/JsonGenerateBuilder'
 import MdHelper from './helpers/MdHelper'
 import CSVBuilder from './base/CSVBuilder'
 
 let path = Utils.getSavePath()
-function buildCsvBodyFun (index, decision, file, bodyString): string[] {
-  let lastStatus = Status.getLatestStatus(path + file.relativePath)
-  let body = `${index}, ${decision}, ${moment(file.mtime).format('YYYY-MM-DD')}, ${lastStatus}\n`
-  return bodyString.push(body)
-}
-
-function outputCsv () {
-  let path = Utils.getSavePath()
-  let i18n = Utils.getI18n()
-  let graphGenerate = new GenerateBuilder(path)
-  let startString = `Index, ${i18n.decision}, ${i18n.modifiedDate}, ${i18n.lastStatus}\n`
-  let results = graphGenerate
-    .setStart(startString)
-    .setEnd('')
-    .setBody(buildCsvBodyFun)
-    .build()
-
-  return results
-}
 
 function buildJsonBodyFun (index, decision, file, bodyString): string[] {
   let lastStatus = Status.getLatestStatus(path + file.relativePath)
