7493be61274de09b0dae807a5064dfcd1494cfdd
Fengda HUANG <h@phodal.com>
2017-11-29 20:32:00 +0800

    [TEST-FIX] try to fix it

--- config/exports/rollup.config.js
+++ config/exports/rollup.config.js
@@ -3,12 +3,12 @@ import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
-    entry: 'build/module/index.js',
-    sourceMap: true,
-    plugins: [
-        nodeResolve({
-            browser: true
-        }),
-        commonjs()
-    ]
+  entry: 'build/module/index.js',
+  sourceMap: true,
+  plugins: [
+    nodeResolve({
+      browser: true
+    }),
+    commonjs()
+  ]
 }
