d17994835c189df8834cedc45346c3a528059344
Phodal HUANG <h@phodal.com>
2018-01-10 08:10:27 +0800

    [t] typo

--- src/tests/generate.spec.ts
+++ src/tests/generate.spec.ts
@@ -76,7 +76,7 @@ test('ADR: generate toc', t => {
   let results = ADR.generate('toc')
   console.log(results)
   t.deepEqual(results,
-`# 架构决策纪录
+`# 架构决策记录
 
 * [1. 编写完整的单元测试](001-编写完整的单元测试.md)
 * [2. 编写完整的单元测试](002-编写完整的单元测试.md)`)
