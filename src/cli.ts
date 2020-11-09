5e46ad38429c4cfa2c0310a25bbf27be854aa77c
Mathieu Lavigne <mathieu.lavigne@compta.com>
2020-11-09 11:15:02 +0100

    FIX #25
    How to properly use command syntax

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
-let program = require('commander')
 let colors = require('colors')
 let version = require('../../package.json').version
+const program = require('commander')
+  .version(version)
 
 let {create} = require('./lib/create')
 let {list} = require('./lib/list')
@@ -15,17 +16,70 @@ let {search} = require('./lib/search')
 let {status} = require('./lib/status')
 
 program
-  .version(version)
-  .usage('[options]')
-  .option('-n, new <title...>', 'create new ADR', create)
-  .option('-l, list', 'list all ADR', list)
-  .option('-u, update', 'update ADR', update)
-  .option('-S, status <index>', 'change one ADR status', status)
-  .option('-g, generate <type>', 'generate toc or graph, default toc', generate)
-  .option('-init, init <language>', 'init ADR with language, e.g. ``adr init en``', init)
-  .option('-logs, logs <index>', 'list one ADR status logs', logs)
-  .option('-o, export <format>', 'export ADR reporter in HTML, CSV, JSON, Markdown', output)
-  .option('-s, search <keywords>', 'search ADRs by keywords', search)
+  .command('new <title...>')
+  .alias('n')
+  .description('create new ADR', {
+    title: 'Title of the new ADR to generate'
+  })
+  .action(titles => {
+    titles.forEach(title => create(title))
+  })
+
+program
+  .command('list')
+  .alias('l')
+  .description('list all ADR')
+  .action(list)
+
+program
+  .command('update')
+  .alias('u')
+  .description('update ADR')
+  .action(update)
+
+program
+  .command('status <index>')
+  .alias('S')
+  .description('change one ADR status', {
+    index: 'Index of the ADR (see ADR file prefix)'
+  })
+  .action(status)
+
+program
+  .command('generate <type>')
+  .alias('g')
+  .description('generate toc or graph, default toc', {
+    type: 'toc | graph'
+  })
+  .action(generate)
+
+program
+  .command('init <language>')
+  .alias('i')
+  .description('init ADR with language, e.g. ``adr init en``')
+  .action(init)
+
+program
+  .command('logs <index>')
+  .alias('L')
+  .description('list one ADR status logs', {
+    index: 'Index of the ADR (see ADR file prefix)'
+  })
+  .action(logs)
+
+program
+  .command('export <format>')
+  .alias('o')
+  .description('export ADR reporter in HTML, CSV, JSON, Markdown', {
+    format: 'csv | json | html'
+  })
+  .action(output)
+
+program
+  .command('search <keywords>')
+  .alias('s')
+  .description('search ADRs by keywords')
+  .action(search)
 
 program.parse(process.argv)
 
