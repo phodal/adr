09a7c391f1c3b3bd7bbcbee15a5d9788f91f8135
Erick Rodriguez <tbogard@outlook.com>
2022-09-21 14:24:54 -0400

    adding language string and adding it to the i18n object

--- src/lib/helpers/i18n.ts
+++ src/lib/helpers/i18n.ts
@@ -1,121 +1,127 @@
-import Config from '../Config'
+import Config from "../Config";
 
-export function getI18n () {
-  let language = Config.getLanguage()
+export function getI18n() {
+  let language = Config.getLanguage();
   let I18N = {
     en: {
-      decision: 'Decision',
-      Status: 'Status',
-      statusStr: 'proposed/accepted/done/deprecated/superseded',
-      modifiedDate: 'Last Modified Date',
-      lastStatus: 'Last Status',
-      logSavePath: 'Save Path:',
-      tocHeader: 'Architecture Decision Records',
+      decision: "Decision",
+      Status: "Status",
+      statusStr: "proposed/accepted/done/deprecated/superseded",
+      modifiedDate: "Last Modified Date",
+      lastStatus: "Last Status",
+      logSavePath: "Save Path:",
+      tocHeader: "Architecture Decision Records",
       status: {
-        proposed: 'Proposed',
-        accepted: 'Accepted',
-        done: 'Done',
-        deprecated: 'Deprecated',
-        superseded: 'Superseded'
-      }
+        proposed: "Proposed",
+        accepted: "Accepted",
+        done: "Done",
+        deprecated: "Deprecated",
+        superseded: "Superseded",
+      },
+      language,
     },
     fa: {
-      decision: 'تصمیم',
-      Status: 'وضعیت',
-      statusStr: 'پیشنهاد شده/پذیرفته شده/پیاده شده/منسوخ/جایگزین شده',
-      modifiedDate: 'آخرین تاریخ ویرایش',
-      lastStatus: 'آخرین وضعیت',
-      logSavePath: 'مسیر ذخیره سازی:',
-      tocHeader: 'مدارک تصمیمات معماری',
+      decision: "تصمیم",
+      Status: "وضعیت",
+      statusStr: "پیشنهاد شده/پذیرفته شده/پیاده شده/منسوخ/جایگزین شده",
+      modifiedDate: "آخرین تاریخ ویرایش",
+      lastStatus: "آخرین وضعیت",
+      logSavePath: "مسیر ذخیره سازی:",
+      tocHeader: "مدارک تصمیمات معماری",
       status: {
-        proposed: 'پیشنهاد شده',
-        accepted: 'پذیرفته شده',
-        done: 'پیاده شده',
-        deprecated: 'منسوخ',
-        superseded: 'جایگزین شده'
-      }
+        proposed: "پیشنهاد شده",
+        accepted: "پذیرفته شده",
+        done: "پیاده شده",
+        deprecated: "منسوخ",
+        superseded: "جایگزین شده",
+      },
+      language,
     },
-    'zh-cn': {
-      decision: '决策',
-      Status: '状态',
-      statusStr: '提议/通过/完成/已弃用/已取代',
-      modifiedDate: '上次修改时间',
-      lastStatus: '最后状态',
-      logSavePath: '保存路径：',
-      tocHeader: '架构决策记录',
+    "zh-cn": {
+      decision: "决策",
+      Status: "状态",
+      statusStr: "提议/通过/完成/已弃用/已取代",
+      modifiedDate: "上次修改时间",
+      lastStatus: "最后状态",
+      logSavePath: "保存路径：",
+      tocHeader: "架构决策记录",
       status: {
-        proposed: '提议',
-        accepted: '通过',
-        done: '完成',
-        deprecated: '已弃用',
-        superseded: '已取代'
-      }
+        proposed: "提议",
+        accepted: "通过",
+        done: "完成",
+        deprecated: "已弃用",
+        superseded: "已取代",
+      },
+      language,
     },
-    'pt-br': {
-      decision: 'Decisão',
-      Status: 'Status',
-      statusStr: 'proposto/aceito/finalizado/descontinuado/cancelado',
-      modifiedDate: 'Data da última modificação',
-      lastStatus: 'Último status',
-      logSavePath: 'Salvo em:',
-      tocHeader: 'Registros de Decisão de Arquitetura',
+    "pt-br": {
+      decision: "Decisão",
+      Status: "Status",
+      statusStr: "proposto/aceito/finalizado/descontinuado/cancelado",
+      modifiedDate: "Data da última modificação",
+      lastStatus: "Último status",
+      logSavePath: "Salvo em:",
+      tocHeader: "Registros de Decisão de Arquitetura",
       status: {
-        proposed: 'Proposto',
-        accepted: 'Aceito',
-        done: 'Finalizado',
-        deprecated: 'Descontinuado',
-        superseded: 'Cancelado'
-      }
+        proposed: "Proposto",
+        accepted: "Aceito",
+        done: "Finalizado",
+        deprecated: "Descontinuado",
+        superseded: "Cancelado",
+      },
+      language,
     },
-    'it-IT': {
-      decision: 'Decisione',
-      Status: 'Stato',
-      statusStr: 'proposta/accettata/implementata/deprecata/sostituita',
-      modifiedDate: 'Ultima Modifica',
-      lastStatus: 'Stato più recente',
-      logSavePath: 'Cartella di salvataggio:',
-      tocHeader: 'Registro Decisioni Architetturali',
+    "it-IT": {
+      decision: "Decisione",
+      Status: "Stato",
+      statusStr: "proposta/accettata/implementata/deprecata/sostituita",
+      modifiedDate: "Ultima Modifica",
+      lastStatus: "Stato più recente",
+      logSavePath: "Cartella di salvataggio:",
+      tocHeader: "Registro Decisioni Architetturali",
       status: {
-        proposed: 'Proposta',
-        accepted: 'Accettata',
-        done: 'Implementata',
-        deprecated: 'Deprecata',
-        superseded: 'Sostituita'
-      }
+        proposed: "Proposta",
+        accepted: "Accettata",
+        done: "Implementata",
+        deprecated: "Deprecata",
+        superseded: "Sostituita",
+      },
+      language,
     },
-    'fr': {
-      decision: 'Décision',
-      Status: 'Statut',
-      statusStr: 'propose/accepte/termine/deprecie/remplace',
-      modifiedDate: 'Date dernière modif.',
-      lastStatus: 'Dernier statut',
-      logSavePath: 'Chemin de sauvegarde:',
-      tocHeader: 'Compte-rendu de Décision d’Architecture',
+    fr: {
+      decision: "Décision",
+      Status: "Statut",
+      statusStr: "propose/accepte/termine/deprecie/remplace",
+      modifiedDate: "Date dernière modif.",
+      lastStatus: "Dernier statut",
+      logSavePath: "Chemin de sauvegarde:",
+      tocHeader: "Compte-rendu de Décision d’Architecture",
       status: {
-        propose: 'Proposé',
-        accepte: 'Accepté',
-        termine: 'Terminé',
-        deprecie: 'Déprécié',
-        remplace: 'Remplacé'
-      }
+        propose: "Proposé",
+        accepte: "Accepté",
+        termine: "Terminé",
+        deprecie: "Déprécié",
+        remplace: "Remplacé",
+      },
     },
     ru: {
-      decision: 'Решение',
-      Status: 'Статус',
-      statusStr: 'предложено/принято/реализовано/устарело/изменено',
-      modifiedDate: 'Дата последнего изменения',
-      lastStatus: 'Последний статус',
-      logSavePath: 'Путь хранения логов:',
-      tocHeader: 'Лог архитектурных решений',
+      decision: "Решение",
+      Status: "Статус",
+      statusStr: "предложено/принято/реализовано/устарело/изменено",
+      modifiedDate: "Дата последнего изменения",
+      lastStatus: "Последний статус",
+      logSavePath: "Путь хранения логов:",
+      tocHeader: "Лог архитектурных решений",
       status: {
-        proposed: 'Предложено',
-        accepted: 'Принято',
-        done: 'Реализовано',
-        deprecated: 'Устарело',
-        superseded: 'Изменено'
-      }
+        proposed: "Предложено",
+        accepted: "Принято",
+        done: "Реализовано",
+        deprecated: "Устарело",
+        superseded: "Изменено",
+      },
+      language,
     },
-  }
+  };
 
-  return I18N[language]
+  return I18N[language];
 }
